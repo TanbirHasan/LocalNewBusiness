7bb117ddd9002b59091b1d285fe77c985c841427
unknown <mdtanbir576@gmail.com>
2022-09-20 20:59:25 +0600

    changed the link

--- components/Navbar.js
+++ components/Navbar.js
@@ -1,26 +1,30 @@
 import React from "react";
 import Link from "next/link";
+import Image from "next/image";
 
 const Navbar = () => {
   return (
-    <div className="py-5 bg-indigo-500 ...">
+    <div className="flex justify-between py-5 px-20 bg-indigo-500 ...">
+      <div className="logo">
+        <Image src="/assets/logo.png" width="120" height="50" alt="logo" />
+      </div>
       <nav>
         <ul className="flex justify-evenly">
           <Link href="/">
-            <li className="menu-link">Home</li>
+            <li className="menu-link">Start Search</li>
           </Link>
           <Link href="/about">
             <li className="menu-link">About</li>
           </Link>
           <Link href="/registration">
-            <li className="menu-link">Registration</li>
+            <li className="menu-link">Contact</li>
           </Link>
           <Link href="/signin">
-            <li className="menu-link">SignIn</li>
+            <li className="menu-link">Login</li>
           </Link>
-          <Link href="/newpassword">
+          {/* <Link href="/newpassword">
             <li className="menu-link">New password</li>
-          </Link>
+          </Link> */}
         </ul>
       </nav>
     </div>
